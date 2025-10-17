cd77564ac22f8fb637764f6de048900c74ee97ed
Harendra Kumar <harendra@composewell.com>
2025-10-17 12:47:40 +0530

    Update the sample flake file

--- flake.bundle.nix
+++ flake.bundle.nix
@@ -7,12 +7,11 @@
     #nixpkgs.url = "github:NixOS/nixpkgs/b2a3852bd078e68dd2b3dfa8c00c67af1f0a7d20"; # nixpkgs-25.05
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/c3d456aad3a84fcd76b4bebf8b48be169fc45c31"; # nixpkgs-25.05-darwin
     #nixpack.url = "path:.";
-    nixpack.url = "github:composewell/nixpack/b3db598aa29533646b13a94aca3fee8ead622d06";
+    nixpack.url = "github:composewell/nixpack/ad487df8591329ef76ce38b3d0fcf7236991a963";
   };
 
   outputs = { self, nixpkgs, nixpkgs-darwin, nixpack }:
-    let nixpack = import "${nixpack}/nix";
-    in nixpack.flakeOutputs {
+    nixpack.flakeOutputs {
       inherit nixpkgs;
       inherit nixpkgs-darwin;
       nixpkgsOptions = {
