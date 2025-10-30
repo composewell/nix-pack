401f284b2603eb530374b0da9bf1286f8a2fab48
Harendra Kumar <harendra@composewell.com>
2025-10-30 19:45:19 +0530

    Update the sample default.nix and flake.nix

--- flake.bundle.nix
+++ flake.bundle.nix
@@ -3,31 +3,29 @@
 
   # XXX it downloads both of these on both systems.
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/branch-off-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/de69d2ba6c70e747320df9c096523b623d3a4c35"; # nixpkgs-unstable
     #nixpkgs.url = "github:NixOS/nixpkgs/b2a3852bd078e68dd2b3dfa8c00c67af1f0a7d20"; # nixpkgs-25.05
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/c3d456aad3a84fcd76b4bebf8b48be169fc45c31"; # nixpkgs-25.05-darwin
-    #nixpack.url = "path:.";
-    nixpack.url = "github:composewell/nixpack/ad487df8591329ef76ce38b3d0fcf7236991a963";
+    #nixpkgs.url = "github:NixOS/nixpkgs/branch-off-24.11";
+
+    # Runs into error: darwin.apple_sdk_11_0 has been removed ...
+    #nixpkgs-darwin.url = "github:NixOS/nixpkgs/e99366c665bdd53b7b500ccdc5226675cfc51f45"; # nixpkgs-unstable
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/08478b816182dc3cc208210b996294411690111d"; # nixpkgs-25.05-darwin
+    # For local testing use "path:.../nixpack";
+    basepkgs.url = "github:composewell/nixpack/796a47f5d9abd9f66c9340e2b9ff9aeed2af1389";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-darwin, nixpack }:
-    nixpack.mkOutputs {
+  outputs = { self, nixpkgs, nixpkgs-darwin, basepkgs }:
+    basepkgs.nixpack.mkOutputs {
       inherit nixpkgs;
       inherit nixpkgs-darwin;
-      inherit nixpack;
-      nixpkgsOptions = {
-            config.allowUnfree = true;
-            config.allowBroken = true;
-          };
-      envOptions = {
-            name = "nixpack-flake";
-            sources = import ./sources.nix {inherit nixpack;};
-            packages = import ./packages.nix;
-            # Use default to utilize the cache,
-            # specific compiler for reproducibility
-            compiler = "default";
-            hoogle = false;
-            isDev = true;
-      };
+      inherit basepkgs;
+      name = "nixpack-flake";
+      sources = import ./sources.nix;
+      packages = import ./packages.nix;
+      # Use default to utilize the cache,
+      # specific compiler for reproducibility
+      #compiler = "default";
+      #installHoogle = false;
+      #isDocs = false;
     };
 }
